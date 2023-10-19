b7a614e8dd1f6fdfa0e720a42701694c9c6159c1
freddiebell88 <frederikcwbell@gmail.com>
2023-10-18 20:30:56 -0400

    click event for search button

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let url = 'https://itunes.apple.com/search?term=mika&entity=allArtist&attribute=allArtistTerm'
 
+
+searchButton.addEventListener('click', () => {
 fetch(url)
 .then((response) => {
     console.log(response)
+    return response.json
+})
 })
