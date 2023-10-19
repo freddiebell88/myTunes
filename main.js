d24d063fffb4249195601046d71282dd39fe0c8f
freddiebell88 <frederikcwbell@gmail.com>
2023-10-19 11:28:45 -0400

    built and called function to add song name results to results display

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-let url = 'https://itunes.apple.com/search?term=mika&entity=allArtist&attribute=allArtistTerm'
+let url = 'https://itunes.apple.com/search?term=dua+lipa&entity=song&limit=20'
 
+let searchResults = document.querySelector('#searchResults')
 
 searchButton.addEventListener('click', () => {
 fetch(url)
-.then((response) => {
-    console.log(response)
-    return response.json
-}).then((data) => {
-    let artist = data.results
-    let newDiv = document.createElement('div')
-    newDiv.innerText = `${artist.artistName} ${artist.primaryGenreName}`
-})
+    .then((response) => {
+        console.log(response)
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        console.log(data.results)
+        console.log(data.results[0].artistName)
+        // let artist = data.results[0]
+        // let result = document.createElement('div')
+        // result.innerText = `${artist.artistName} ${artist.primaryGenreName} ${artist.trackName}`
+        // searchResults.appendChild(result)
+        buildResults(data.results)
+    })
 
 })
+
+function buildResults(musicArray) {
+    for (let song of musicArray) {
+        let songBox = document.createElement('div')
+        songBox.classList.add('result')
+        let songName = document.createElement('h4')
+        songName.innerText = song.trackName
+        songBox.appendChild(songName)
+        searchResults.appendChild(songBox) 
+    }
+}
+
